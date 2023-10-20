132d5b9cfcd548cc1e39c11f6d069e9bde7f2e30
jeanclaudio <jeanclaudio96@hotmail.com>
2023-10-20 14:23:32 -0300

    Refatoração de controlers para dar respostas rest para o frontend server e permitir requisicoes pela porta localhost/3000

--- src/main/java/com/EstoqueTL/Configuration/SecurityConfiguration.java
+++ src/main/java/com/EstoqueTL/Configuration/SecurityConfiguration.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -12,10 +17,22 @@ public class SecurityConfiguration {
 
 	@Bean
 	public SecurityFilterChain filterchain(HttpSecurity http) throws Exception {
-		http.
-				authorizeHttpRequests((authorizeHttpRequests) -> 
-					authorizeHttpRequests
-						.requestMatchers("/**").permitAll());
+		http
+			.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests((authorizeHttpRequests) ->
+						authorizeHttpRequests
+								.requestMatchers("/**").permitAll());
 		return http.build();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // Adicione aqui a URL do seu frontend React
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
--- src/main/java/com/EstoqueTL/Controllers/root/RequisicaoController.java
+++ src/main/java/com/EstoqueTL/Controllers/root/RequisicaoController.java
@@ -4,13 +4,11 @@ import com.EstoqueTL.Data.DTO.RequisicaoDTO;
 import com.EstoqueTL.Data.Models.Estoque;
 import com.EstoqueTL.Data.Repositorys.EstoqueRepository;
 import com.EstoqueTL.Services.RequisicaoService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.EstoqueTL.Data.Models.Requisicao;
 import com.EstoqueTL.Data.Repositorys.RequisicaoRepository;
@@ -19,7 +17,8 @@ import jakarta.validation.Valid;
 
 import java.util.List;
 
-@Controller
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(value="/")
 public class RequisicaoController {
 
@@ -34,27 +33,24 @@ public class RequisicaoController {
     }
 
 	@GetMapping
-	public String RequisicaoGet(Model model) {
+	public ResponseEntity<List<Estoque>> RequisicaoGet(Model model) {
 
 		List<Estoque> estoqueList = (List<Estoque>) estoqueRepository.findAll();
-		model.addAttribute("estoqueList", estoqueList);
-		
-		return "root/requisicaoPage";
+		return ResponseEntity.ok(estoqueList);
 	}
 	
 	@PostMapping
-	public String RequisicaoPost(Model model, @Valid @ModelAttribute RequisicaoDTO requisicaoDTO, BindingResult bindingResult) {
+	public ResponseEntity<?> RequisicaoPost(Model model, RequisicaoDTO requisicaoDTO, BindingResult bindingResult) {
 
 		Requisicao requisicao = new Requisicao();
 
 		if (bindingResult.hasErrors()) {
-			return "root/requisicaoPage";
+			return ResponseEntity.badRequest().body("Erro ao enviar o formulario");
 			}
 
 		requisicao = requisicaoService.convertDtoToEntity(requisicaoDTO);
-		
 		requisicaoRepository.save(requisicao);
 		
-		return "redirect:/minhasRequisicoes";
+		return ResponseEntity.ok("Requisicao enviada com sucesso");
 	}
 }
